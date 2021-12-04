2c7258c7ae6f8726dd18b7ee91d4bb9d9fcf430b
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2021-12-04 16:53:17 -0500

    added window prompts and alerts

--- Develop/script.js
+++ Develop/script.js
@@ -7,22 +7,46 @@ var generateBtn = document.querySelector("#generate");
 var charUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 var charLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var charSpecial = [',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '!', '#', '$', '%', '&', '*']
-var charNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+var charNumber = ['0','1','2','3','4','5','6','7','8','9']
 
-function generatePassword() {
-  console.log("this button works");
+function generateOptions() {
 
-//Prompt for password LENGTH (8-128)
+  //Prompt for password LENGTH (8-128)
+  let passwordLength = parseInt(window.prompt("How many characters (between 8-128) would you like your password to contain"));
 
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+    window.alert("Please enter a valid number between 8 and 128.")
+    return;
+  }
 
-//Prompt user for uppercase, lowercase, numeric, special characters
+  //Prompt user for uppercase, lowercase, numeric, special characters
+  var includeUpper = window.confirm("Would you like to include uppercase letters?");
+  var includeLower = window.confirm("Would you like to include lowercase letters?");
+  var includeSpecial = window.confirm("Would you like to include special characters?");
+  var includeNumber = window.confirm("Would you like to include numbers?");
 
+  //Validate the user's input (make sure length is between 8-128 and that at least one character type is selected)
+  if (!includeUpper && !includeLower && !includeSpecial && !includeNumber) {
+    window.alert("Please choose at least one value to use the password generator.")
+    return;
+  }
 
-//Validate the user's input (make sure length is between 8-128 and that at least one character type is selected)
+  var passwordOptions = {
+    passwordLength: passwordLength,
+    charUpper: includeUpper,
+    charLower: includeLower,
+    charSpecial: includeSpecial,
+    charNumber: includeNumber
+  }
 
+  console.log (passwordOptions);
+
+}
+
+generateOptions();
 
 //Generate the password
-
+function generatePassword() {
 
 //Display the generate password to the page
   return "password goes here";
