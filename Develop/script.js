014949a03898a096a9e2cb71560c152d4c1a6f15
Tamara Wilhite <wilhite.tamara6452@gmail.com>
2021-12-04 18:58:51 -0500

    finalized generatepassword function to finalize js file

--- Develop/script.js
+++ Develop/script.js
@@ -1,67 +1,67 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var charUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-var charLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-var charSpecial = [',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '!', '#', '$', '%', '&', '*']
-var charNumber = ['0','1','2','3','4','5','6','7','8','9']
+var charUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var charLower = "abcdefghijklmnopqrstuvwxyz"
+var charSpecial = "!@#$%^&*()<>.,?:;+="
+var charNumber = "0123456789"
 
-function generateOptions() {
+function generatePassword() {
 
   //Prompt for password LENGTH (8-128)
   let passwordLength = parseInt(window.prompt("How many characters (between 8-128) would you like your password to contain"));
 
+  //Validate user's input (give user an alert that they need to choose valid character length (between 8-128, a number)
   if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     window.alert("Please enter a valid number between 8 and 128.")
-    return;
+    return generatePassword();
   }
 
-  //Prompt user for uppercase, lowercase, numeric, special characters
-  var includeUpper = window.confirm("Would you like to include uppercase letters?");
-  var includeLower = window.confirm("Would you like to include lowercase letters?");
-  var includeSpecial = window.confirm("Would you like to include special characters?");
-  var includeNumber = window.confirm("Would you like to include numbers?");
+  //Prompt user to choose if they want uppercase, lowercase, numeric, special characters in their passwords and how to select
+  var chooseUpper = window.confirm("Would you like to include uppercase letters? Click OK for YES or CANCEL for NO.");
+  var chooseLower = window.confirm("Would you like to include lowercase letters? Click OK for YES or CANCEL for NO.");
+  var chooseSpecial = window.confirm("Would you like to include special characters? Click OK for YES or CANCEL for NO.");
+  var chooseNumber = window.confirm("Would you like to include numbers? Click OK for YES or CANCEL for NO.");
 
-  //Validate the user's input (make sure length is between 8-128 and that at least one character type is selected)
-  if (!includeUpper && !includeLower && !includeSpecial && !includeNumber) {
+  //Validate the user's input (give user alert that at least one character type has to be selected if they choose none, then return to start)
+  if (!chooseUpper && !chooseLower && !chooseSpecial && !chooseNumber) {
     window.alert("Please choose at least one value to use the password generator.")
-    return;
+    return generatePassword();
   }
 
-  var passwordOptions = {
-    passwordLength: passwordLength,
-    charUpper: includeUpper,
-    charLower: includeLower,
-    charSpecial: includeSpecial,
-    charNumber: includeNumber
+  //combine the user's choices into one variable based on whether they are true or not (var -> password options)
+  var passwordOptions = "";
+    chooseUpper ? passwordOptions += charUpper: ""; //IF chooseUpper is true, then charUpper string will be added to passwordOptions
+    chooseLower ? passwordOptions += charLower: ""; //IF chooseLower is true, then charLower string will be added to passwordOptions
+    chooseSpecial ? passwordOptions += charSpecial: ""; //IF chooseSpecial is true, then charSpecial string will be added to passwordOptions
+    chooseNumber ? passwordOptions += charNumber: ""; //IF chooseNumber is true, then charNumber string will be added to passwordOptions
+  
+  //Generate the password
+  var finalPassword = "";
+
+  //commands the generator to choose a random character from the string of each variable that the user selected while also meeting the passwordLength criteria set by the user
+  for (var i = 0; i < passwordLength; i++) {
+    finalPassword += passwordOptions.charAt(Math.floor(Math.random() * passwordOptions.length));
   }
 
-  console.log (passwordOptions);
+  //log user selections and final password in the console to verify everything is working right
+  console.log(passwordLength);
+  console.log(passwordOptions);
+  console.log(finalPassword);
 
+  //Display the generated password to the page
+  return finalPassword;
 }
 
-generateOptions();
-
-//Generate the password
-function generatePassword() {
-
-//Display the generate password to the page
-  return "password goes here";
-}
-
-// Write password to the #password input // function that is called by click of generate button // function from "generatePassword"
-//will be stored in Password variable // no generatePassword function is made !!
+// Write password to the #password input 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  //password will be displayed on password text on screen
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button // when generate button is clicked, writePassword function will be called
+// Add event listener to generate button 
 generateBtn.addEventListener("click", writePassword);
